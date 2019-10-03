d249b48bab8d3b55f873ee23d4dd697db6f62dff
Teo <teo.cotx@gmail.com>
2019-10-03 18:14:31 +0200

    Experimentando con colores

--- cosasdejava.java
+++ cosasdejava.java
@@ -2,8 +2,11 @@ public class cosasdejava {
 
 	public static void main(String[] args) {
 
-		System.out.println("otra Cosa");
-		System.out.println("Adios mundo");
+
+		String amarillo = "\033[33m";
+		String verde = "\033[32m";
+		System.out.println(amarillo + "otra Cosa");
+		System.out.println(verde + "Adios mundo");
 	}
 
 }
