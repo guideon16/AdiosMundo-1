7ab8d4cd71615adfe747de6474426bfb6a3ee960
guideon16 <pablomrocker@gmail.com>
2019-10-03 19:37:50 +0200

    Mensaje modificado para el cabron de teo

--- cosasdejava.java
+++ cosasdejava.java
@@ -8,8 +8,7 @@ public class cosasdejava {
 		String reset = "\033[0m";
 		System.out.println(amarillo + "otra Cosa" + reset);
 		System.out.println(verde + "Adios mundo" + reset);
-		
-
+		System.out.println("TEO VETE A TOMAR POR CULO");
 
 	}
 
